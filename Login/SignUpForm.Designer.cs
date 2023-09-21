1308304ba72ed9ce7927a166adcfd942164ab928
vandu <vandu@DESKTOP-CKTBGDM>
2023-09-21 17:19:55 +0700

    Update Sign up Form

---
+++ Login/SignUpForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace QuanLiSinhVien.Login
+{
+    partial class SignUpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignUpForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.AppName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(89, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 149);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AppName
+            // 
+            this.AppName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AppName.AutoSize = true;
+            this.AppName.BackColor = System.Drawing.SystemColors.Highlight;
+            this.AppName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.AppName.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AppName.Location = new System.Drawing.Point(442, 60);
+            this.AppName.Name = "AppName";
+            this.AppName.Size = new System.Drawing.Size(648, 47);
+            this.AppName.TabIndex = 3;
+            this.AppName.Text = "PHẦN MỀM QUẢN LÍ SINH VIÊN";
+            // 
+            // SignUpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1849, 833);
+            this.Controls.Add(this.AppName);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "SignUpForm";
+            this.Text = "Phần mềm quản lí sinh viên - Đăng ký";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label AppName;
+    }
+}
