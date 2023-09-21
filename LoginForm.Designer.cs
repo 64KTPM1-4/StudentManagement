bf1c943b859649b33d7c008f7732f48a1eb6d8b5
vandu <vandu@DESKTOP-CKTBGDM>
2023-09-21 16:13:52 +0700

    Update Login Form

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace QuanLiSinhVien
+{
+    partial class Login_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RegisterLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.Username.Location = new System.Drawing.Point(519, 177);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(489, 33);
+            this.Username.TabIndex = 4;
+            this.Username.Tag = "";
+            this.Username.TextChanged += new System.EventHandler(this.Username_TextChanged);
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.Password.Location = new System.Drawing.Point(519, 230);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(489, 33);
+            this.Password.TabIndex = 5;
+            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(541, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tên đăng nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(541, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.Location = new System.Drawing.Point(519, 281);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(146, 30);
+            this.LoginButton.TabIndex = 8;
+            this.LoginButton.Text = "Đăng nhập";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(515, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Chưa có tài khoản ?";
+            // 
+            // RegisterLabel
+            // 
+            this.RegisterLabel.AutoSize = true;
+            this.RegisterLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterLabel.Location = new System.Drawing.Point(676, 367);
+            this.RegisterLabel.Name = "RegisterLabel";
+            this.RegisterLabel.Size = new System.Drawing.Size(75, 22);
+            this.RegisterLabel.TabIndex = 11;
+            this.RegisterLabel.TabStop = true;
+            this.RegisterLabel.Text = "Đăng ký";
+            // 
+            // Login_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1849, 833);
+            this.Controls.Add(this.RegisterLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Name = "Login_Form";
+            this.Text = "Phần mềm quản lí sinh viên - Đăng nhập";
+            this.Controls.SetChildIndex(this.Username, 0);
+            this.Controls.SetChildIndex(this.Password, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.LoginButton, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.RegisterLabel, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel RegisterLabel;
+    }
+}
