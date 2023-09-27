139c1c223d7efb94e5fda2fd17398aee0c9db77b
Nhoxboon <trannhatanh156@gmail.com>
2023-09-27 23:52:21 +0700

    Update

--- Model/DanhSachAcc.cs
+++ Model/DanhSachAcc.cs
@@ -10,18 +10,18 @@ namespace QuanLiSinhVien.Model
         public DanhSachAccount()
         {
             
-        var accounts = new List<AccountModel>
-        {
-            new AccountModel { username = "Chuyenvien", password = "chuyenvien123", roles = "Chuyen vien" },
-            new AccountModel { username = "GiaovienChuNhiem", password = "GVCN123", roles = "GVCN" },
-            new AccountModel { username = "GiaovienBoMon", password = "GVBM123", roles = "GVBM" },
-            new AccountModel { username = "Loptruong", password = "loptruong123", roles = "Lop truong" }
-        };
+            var accounts = new List<AccountModel>
+            {
+                new AccountModel { username = "Chuyenvien", password = "chuyenvien123", roles = "Chuyen vien" },
+                new AccountModel { username = "GiaovienChuNhiem", password = "GVCN123", roles = "GVCN" },
+                new AccountModel { username = "GiaovienBoMon", password = "GVBM123", roles = "GVBM" },
+                new AccountModel { username = "Loptruong", password = "loptruong123", roles = "Lop truong" }
+            };
 
             
-        string json = JsonConvert.SerializeObject(accounts, Formatting.Indented);
+            string json = JsonConvert.SerializeObject(accounts, Formatting.Indented);
 
-
+            
             File.WriteAllText("Account.json", json);
         }
     }
