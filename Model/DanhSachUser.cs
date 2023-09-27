b51850225f08c55a0c9957828f716400fd4f1dff
Nhoxboon <trannhatanh156@gmail.com>
2023-09-27 20:05:39 +0700

    Update

--- Model/DanhSachUser.cs
+++ Model/DanhSachUser.cs
@@ -1,41 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Windows.Forms;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace QuanLiSinhVien
-
-{
-    public class DanhSachUser
-    {
-        private static DanhSachUser instance;
-        public static DanhSachUser Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new DanhSachUser();
-                }
-                return instance;
-            }
-            set => instance = value;
-        }
-
-        List<Username> listTK;
-
-        public List<Username> ListTK
-        { get; set; }
-
-        DanhSachUser()
-        {
-            listTK = new List<Username>();
-            listTK.Add(new Username("TruongKhoa", "truongkhoa123", 1));
-            listTK.Add(new Username("GVChuNhiem", "CN123456", 2));
-            listTK.Add(new Username("GVBoMon", "BM123456", 3));
-            listTK.Add(new Username("LopTruong", "LT123456", 4));
-        }
-    }
-}
--- Model/Users.cs
+++ Model/Users.cs
@@ -1,33 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Windows.Forms;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace QuanLiSinhVien
-
-{
-    public class Username
-    {
-        private string tenTaiKhoan;
-        private string matKhau;
-
-        public string TenTaiKhoan
-        { get; set; }
-
-        public string MatKhau
-        { get; set; }
-
-        private int loaiTK;
-        public int LoaiTK
-        { get; set; }
-
-        public Username(string tentaikhoan, string matkhau, int loaitk)
-        {
-            this.TenTaiKhoan = tentaikhoan;
-            this.MatKhau = matkhau;
-            this.LoaiTK = loaitk;
-        }
-    }
-}
+namespace QuanLiSinhVien.Model
