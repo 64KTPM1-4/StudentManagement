f9481800fa31da0ff1417a7fec9010a47ad8a373
Nhoxboon <trannhatanh156@gmail.com>
2023-09-27 19:27:43 +0700

    Update

---
+++ Model/Users.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuanLiSinhVien
+
+{
+    public class Username
+    {
+        private string tenTaiKhoan;
+        private string matKhau;
+
+        public string TenTaiKhoan
+        { get; set; }
+
+        public string MatKhau
+        { get; set; }
+
+        private int loaiTK;
+        public int LoaiTK
+        { get; set; }
+
+        public Username(string tentaikhoan, string matkhau, int loaitk)
+        {
+            this.TenTaiKhoan = tentaikhoan;
+            this.MatKhau = matkhau;
+            this.LoaiTK = loaitk;
+        }
+    }
+}
