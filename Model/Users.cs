ee60d9b963d0a249a52906c8e4d820225b0d05f0
Nhoxboon <trannhatanh156@gmail.com>
2023-09-27 20:07:01 +0700

    Update

--- Model/Users.cs
+++ Model/Users.cs
@@ -5,3 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace QuanLiSinhVien.Model
+{
+    public class TaiKhoan
+    {
+        private string tenTaiKhoan;
+
+        public string TenTaiKhoan
+        { get; set; }
+
+        private string matKhau;
+
+        public string MatKhau
+        { get; set; }
+
+        TaiKhoan(string tentaikhoan, string matkhau) 
+        {
+            this.TenTaiKhoan = tentaikhoan;
+            this.MatKhau = matkhau;
+        }
+    }
