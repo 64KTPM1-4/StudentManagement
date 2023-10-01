11ee5c57c8dae26937541f6d0ad6bcef9741b4bc
Nhoxboon <trannhatanh156@gmail.com>
2023-10-01 22:38:19 +0700

    update TeacherModel (dkm cuoc doi)

---
+++ Model/TeacherModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiSinhVien.Model
+{
+    public class TeacherModel
+    {
+        [DisplayName("ID")]
+        public int TeacherId { get; set; } = 0;
+        [DisplayName("Tên giáo viên")]
+        public string TeacherName { get; set; } = string.Empty;
+
+        public List<int> ClassId { get; set; } = new List<int>();
+    }
+}
