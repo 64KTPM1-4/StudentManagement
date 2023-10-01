5a762f903f688a8cd590f63779eff42981afd620
Nhoxboon <trannhatanh156@gmail.com>
2023-10-01 22:09:10 +0700

    update

--- Services/StudentServices.cs
+++ Services/StudentServices.cs
@@ -19,11 +19,11 @@ namespace QuanLiSinhVien.Services
             {
                 string json = File.ReadAllText(@"students.json");
                 studentList = JsonConvert.DeserializeObject<List<StudentModel>>(json);
-        }
+            }
             catch(FileNotFoundException)
-        {
+            {
                 File.WriteAllText(@"students.json", "[]");
-        }
+            }
             return studentList;
         }
 
@@ -33,21 +33,21 @@ namespace QuanLiSinhVien.Services
             int StudentId = 0;
             var studentList = JsonConvert.DeserializeObject<List<StudentModel>>(File.ReadAllText(@"students.json"));
             int index = studentList.Count() - 1;
-
+            
             if(index >= 0) 
-        {
+            {
                 StudentId = studentList[index].Id + 1;
-        }
+            }
             List<StudentModel> newStudent = new List<StudentModel>();
             newStudent.Add(new StudentModel
-        {
+            {
                 Id = StudentId,
                 Name = StudentName,
             });
 
             studentList.AddRange(newStudent);
             File.WriteAllText(@"students.json", JsonConvert.SerializeObject(studentList));
-            }
+        }
 
         public void DeleteStudent(string StudentName)
         {
@@ -56,16 +56,16 @@ namespace QuanLiSinhVien.Services
             var index = studentList.FindIndex(x => x.Name == Name);
             studentList.RemoveAt(index);
             File.WriteAllText(@"students.json", JsonSerializer.Serialize(studentList));
-            }
+        }
 
 
         public void EditStudent(string oldStudentName, string newStudentName)
-            {
+        {
             var studentList = JsonSerializer.Deserialize<List<StudentModel>>(File.ReadAllText(@"stuents.json"));
             var student = studentList.FirstOrDefault(s => s.Name.Equals(oldStudentName, StringComparison.OrdinalIgnoreCase));
 
             if (student != null)
-        {
+            {
                 student.Name = newStudentName;
                 File.WriteAllText(@"students.json", JsonSerializer.Serialize(studentList));
             }
