047e378ec1a988ecd82d941d6ec4bbeef1fdf74f
Nhoxboon <trannhatanh156@gmail.com>
2023-10-04 21:44:47 +0700

    update

--- Class Form/StudentInSubject.Designer.cs
+++ Class Form/StudentInSubject.Designer.cs
@@ -117,7 +117,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1849, 833);
+            this.ClientSize = new System.Drawing.Size(1284, 833);
             this.Controls.Add(this.ReturnButton);
             this.Controls.Add(this.DeleteClassStudentButton);
             this.Controls.Add(this.AddClassStudentButton);
