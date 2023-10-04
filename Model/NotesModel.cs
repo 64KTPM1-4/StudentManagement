8eb51f146811c06542ab5f044523c98677989ad7
Nhoxboon <trannhatanh156@gmail.com>
2023-10-04 22:20:20 +0700

    update NotesModel and PointModel

---
+++ Model/NotesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiSinhVien.Model
+{
+    public class NotesModel
+    {
+        [DisplayName("Vi phạm")]
+        public string Notes { get; set; } = string.Empty;
+    }
+}
---
+++ Services/NoteServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+using QuanLiSinhVien.Model;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace QuanLiSinhVien.Services
+{
+    public class NoteServices
+    {
+        public List<>
+    }
+}
